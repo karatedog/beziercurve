b0665cec9c06fd0441998b2fe565c06cb798ff82
Földes László <foldes.laszlo2@gmail.com>
2016-04-10 17:48:44 +0200

    Rubydoc did not show README, trying to fix it

--- beziercurve.gemspec
+++ beziercurve.gemspec
@@ -1,13 +1,13 @@
 Gem::Specification.new do |s|
   s.name        = 'beziercurve'
-  s.version     = '0.8.1'
-  s.date        = '2015-05-04'
+  s.version     = '0.8.4'
+  s.date        = '2016-04-10'
   s.summary     = "Create and analyze Bézier curves"
   s.description = "Creates a Bézier curve by its control points. Implemented with de Casteljau method."
   s.required_ruby_version = '>= 2.0.0'  
   s.authors     = ["Földes László"]
   s.email       = 'foldes.laszlo2@gmail.com'
-  s.files       = ["lib/beziercurve.rb"]
+  s.files       = Dir["{lib,test}/**/*.{rb,md}"] + Dir["*.{md,rdoc}"]
   s.homepage    = 'https://github.com/karatedog/beziercurve'
-  s.license	= "MIT"
+  s.license	    = "MIT"
 end
--- lib/beziercurve.rb
+++ lib/beziercurve.rb
@@ -1,13 +1,16 @@
 # Curve == series of ControlPoints
 
 module Bezier
-
 	class ControlPoint
 		attr_accessor :x, :y
 
+    # @param x [Numeric]
+    # @param y [Numeric]
+    #
+    # Creates a new control point for the Bézier curve
 		def initialize(x,y)
 			@x = x
-			@y = y		
+			@y = y
 		end
 
 		def - (b)
@@ -19,8 +22,8 @@ module Bezier
 
 		# @param point [ControlPoint]
 		#
-    # @return [ControlPoint] Moves a ControlPoint
-    # Moves Self by the 'point' as relative coordinates
+    # @return [ControlPoint] A ControlPoint in a new position
+    # Moves a controlpoint with relative distance
     def movepoint (point)
 			self.class.new(self.x + point.x, self.y + point.y)
 		end
@@ -34,7 +37,8 @@ module Bezier
 			CurvePoint.new(self.x, self.y)
 		end
 
-    # @return [Array] Returns an Array. The Array is fit to be as argument to Curve.new
+    # @return [Array]
+    # Returns the control point as an array => [x, y]. The Array is fit to be as argument for ControlPoint.new
     def to_a
 			[self.x, self.y]
 		end
@@ -48,29 +52,67 @@ module Bezier
 	end
 
 	class Curve
+    # defaults
+    DeCasteljau = :decasteljau
+    Bernstein = :bernstein
+
+    # this should have been instance variable in the first place, correcting '@@...'
+    @calculation_method = Bernstein
+
+		@@fact_memoize = Hash.new
+		@@binomial_memoize = Hash.new
+		@@pascaltriangle_memoize = Hash.new
+
+		# Ye' olde factorial function
+		#
+		# @param n [Fixnum]
+		# @example
+		#   > fact(5)
+		def self.fact(n)
+			@@fact_memoize[n] ||= (1..n).reduce(:*)
+    end
+
+		# @param n [Fixnum]
+		# @param k [Fixnum]
+		# standard 'n choose k'
+		def self.binomial(n,k)
+			return 1 if n-k <= 0
+			return 1 if k <= 0
+			@@binomial_memoize[[n,k]] ||= fact(n) / ( fact(k) * fact( n - k ) )
+    end
+
+		# Returns the specified line from the Pascal triangle as an Array
+		# @return [Array] A line from the Pascal triangle
+		# @example
+		#   > pascaltriangle(6)
+		def self.pascaltriangle(nth_line) # Classic Pascal triangle
+			@@pascaltriangle_memoize[nth_line] ||= (0..nth_line).map { |e| binomial(nth_line, e) }
+    end
+
 		# Returns the Bezier curve control points
 		#
 		# @return [Array<ControlPoints>]
+
 		attr_accessor :controlpoints
 
-		# @param controlpoints [Array<ControlPoints>, Array<(Fixnum, Fixnum)>] list of ControlPoints defining the hull for the Bézier curve. A point can be of class ControlPoint or an Array containig 2 Fixnums, which will be converted to ControlPoint.
-		# @return [Curve] a Bézier curve object. The minimum number of control points is 3.
+		# @param controlpoints [Array<ControlPoints>, Array<(Fixnum, Fixnum)>] list of ControlPoints defining the hull for the Bézier curve. A point can be of class ControlPoint or an Array containig 2 Numerics, which will be converted to ControlPoint.
+		# @return [Curve] Creates a new Bézier curve object. The minimum number of control points is currently 3.
 		# @example
 		#    initialize(p1, p2, p3)
 		#    initialize(p1, [20, 30], p3)
 		def initialize(*controlpoints)
-			
+
 			# need at least 3 control points
 			# this constraint has to be lifted, to allow adding Curves together like a 1 point curve to a 3 point curve
 			if controlpoints.size < 3
-				raise ArgumentError, 'Cannot create Bézier curve with less than 3 control points'
+				raise ArgumentError, 'Cannot create curve with less than 3 control points'
 			end
 
-			@controlpoints = controlpoints.map { |e|
-				if e.class == Array
-					ControlPoint.new(*e[0..1]) # make sure ControlPoint.new gets no more than 2 arguments. 'e' should contain at least 2 elements here
-				elsif e.class == ControlPoint
-					e
+			@controlpoints = controlpoints.map { |point|
+				if point.class == Array
+					ControlPoint.new(*point[0..1]) # ControlPoint.new gets no more than 2 arguments, excess values are ignored
+				elsif point.class == ControlPoint
+					point
 				else
 					raise 'Control points should be type of ControlPoint or Array'
 				end
@@ -79,85 +121,94 @@ module Bezier
 
 		# Adds a new control point to the Bezier curve as endpoint.
 		#
-		# @param [ControlPoint] point
+		# @param [ControlPoint, Array] point
 		def add(point)
-			if point.class == ControlPoint
-				@controlpoints << point
-			else
-				raise TypeError, 'Point should be type of ControlPoint'
-			end
+      @controlpoints << case point
+                        when ControlPoint
+                          point
+                        when Array
+                          ControlPoint.new(*point[0..1])
+                        else
+                          raise(TypeError, 'Point should be type of ControlPoint')
+                        end
+
+			# if point.class == ControlPoint
+			# 	@controlpoints << point
+   #    elsif point.class == Array
+   #      @controlpoints << ControlPoint.new(*point[0..1])
+			# else
+			# 	raise TypeError, 'Point should be type of ControlPoint'
+			# end
 		end
 
 		# @param [CurvePoint] t
-		def point_on_curve(t) # calculates the 'x,y' coordinates of a point on the curve, at the ratio 't' (remember, 0 <= t <= 1)
+		def point_on_curve(t) # calculates the 'x,y' coordinates of a point on the curve, at the ratio 't' (0 <= t <= 1)
+      case
+        when @@calculation_method == DeCasteljau
+          poc_with_decasteljau(t)
+        when @@calculation_method == Bernstein
+          poc_with_bernstein(t)
+        else
+          poc_with_bernstein(t) # default
+      end
+    end
+    alias_method :poc, :point_on_curve
 
-			def point_on_hull(point1, point2, t) # making this method local
-				if (point1.class != ControlPoint) or (point2.class != ControlPoint)
-					raise TypeError, 'Both points should be type of ControlPoint'
-				end
-				new_x = (point1.x - point2.x) * t
-				new_y = (point1.y - point2.y) * t
-				return ControlPoint.new(new_x, new_y)
-			end
+		def poc_with_bernstein(t)
+			n = @controlpoints.size-1
+      sum = [0,0]
+      for k in 0..n
+      	sum[0] += @controlpoints[k].x * self.class.binomial(n,k) * (1-t)**(n-k) * t**k
+      	sum[1] += @controlpoints[k].y * self.class.binomial(n,k) * (1-t)**(n-k) * t**k
+      end
+      return ControlPoint.new(*sum)
+      #poc_with_decasteljau(t)
+    end
 
-			# imperatively ugly, but works, refactor later. point_on_curve and point_on_hull should be one method
-			ary = @controlpoints
-			return ary if ary.length <= 1 # zero or one element as argument, return unmodified
+    def point_on_hull(point1, point2, t) # just realized this was nested (geez), Jörg.W.Mittag would have cried. So it is moved out from poc_with_decasteljau()
+      if (point1.class != ControlPoint) or (point2.class != ControlPoint)
+        raise TypeError, 'Both points should be type of ControlPoint'
+      end
+      new_x = (1-t) * point1.x + t * point2.x
+      new_y = (1-t) * point1.y + t * point2.y
+      return ControlPoint.new(new_x, new_y)
+    end
 
-			while ary.length > 1
-				temp = []
-				0.upto(ary.length-2) do |index|
-					memoize1 = point_on_hull(ary[index], ary[index+1], t)
-					temp << ary[index+0] - memoize1
-				end
-				ary = temp
-			end
-			temp[0].to_curvepoint
-		end
+    def poc_with_decasteljau(t)
+      # imperatively ugly, but works, refactor later. point_on_curve and point_on_hull should be one method
+      ary = @controlpoints
+      return ary if ary.length <= 1 # zero or one element as argument, return unmodified
 
-		# Ye' olde factorial function
-		#
-		# @param n [Fixnum] 
-		# @todo this is slow, should be rewritten
-		# @example
-		#   > fact(5)
-		def self.fact(n)
-			(1..n).reduce(:*)
-		end
-
-		# @param n [Fixnum]
-		# @param k [Fixnum]
-		# standard 'n choose k'
-		def self.binomial(n,k) 
-			return 1 if n-k <= 0
-			return 1 if k <= 0
-			fact(n) / ( fact(k) * fact( n - k ) )
-		end
-
-		# Returns the specified line from the Pascal triangle as an Array
-		# @todo memoize already created lines or precalculate a few ten lines
-		# @return [Array] A line from the Pascal triangle
-		def self.pascaltriangle(nth_line) # Classic Pascal triangle
-			(0..nth_line).map { |e| binomial(nth_line, e) }
-		end
+      while ary.length > 1
+        temp = []
+        0.upto(ary.length-2) do |index|
+          memoize1 = point_on_hull(ary[index], ary[index+1], t)
+          temp << ary[index+0] - memoize1
+        end
+        ary = temp
+      end
+      temp[0].to_curvepoint
+    end
 
 		def point_on_curve_binom(t)
-
-			# locally scoped, we don't need it outside of point_on_curve_binom
-
 			coeffs = self.class.pascaltriangle(self.order)
-			coeffs.reduce { |memo, obj|
+			coeffs.reduce do |memo, obj|
 				memo += t**obj * (1-t)** (n - obj)
-			}
+			end
 		end
 
-		def display_points # just a helper, for quickly put ControlPoints to STDOUT in a gnuplottable format
-			@controlpoints.map{|point| puts "#{point.x} #{point.y}"}
+
+		def gnuplot_hull # was recently 'display_points'. just a helper, for quickly put ControlPoints to STDOUT in a gnuplottable format
+			@controlpoints.map{|point| [point.x, point.y] }
+		end
+
+		def gnuplot_points(precision)
 		end
 
 		# returns a new Enumerator that iterates over the Bezier curve from [start_t] to 1 by [delta_t] steps.
 		def enumerated(start_t, delta_t)
 	  		Enumerator.new do |yielder|
+	  			#TODO only do the conversion if start_t is not Float, Fractional or Bigfloat
 	  			point_position = start_t.to_f
 	    		number = point_on_curve(point_position)
 	    		loop do
@@ -175,3 +226,9 @@ module Bezier
 		end
 	end
 end
+
+# Bernstein calculation is finished, that is the default
+# DeCasteljau still has bugs 
+# renamed display_points to gnuplot_hull (this displays Hull coordinates)
+# created gnuplot_points (this displays Curve points, uses the enumerated method to iterate over the curve)
+# TODO: make sure the original CurvePoint argument type won't get converted. If someone created a new Bezier instance with Bigfloat as CurvePoints, it should stay Bigfloat during the entire calculation
